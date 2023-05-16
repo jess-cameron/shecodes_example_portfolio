2ac235f2ebf754f7499a1d9361b9f7f00d55a0a1
Jessica Cameron <jessicaellencameron@gmail.com>
2023-05-16 19:18:51 +1000

    Initial shecodes portfolio code

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const hamburgerButton = document.querySelector(".hamburger-button");
+const mobileNav = document.querySelector(".mobile-nav")
+hamburgerButton.addEventListener("click", () => {
+    mobileNav.classList.toggle("display-none")
+})
